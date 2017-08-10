7a83f99117896a3492cc6784cd07f49d1167c59b
Milind <milind.db88@gmail.com>
2017-08-10 11:13:45 +0530

    differentiated the jar and war configuration of SpringBootApplication

--- spring-boot-sample/src/main/java/com/milind/springbootsample/SpringBootSampleApplication.java
+++ spring-boot-sample/src/main/java/com/milind/springbootsample/SpringBootSampleApplication.java
@@ -1,6 +1,5 @@
 package com.milind.springbootsample;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
@@ -9,15 +8,6 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 // to scan JPA entities in project
 // @EntityScan(basePackages = "com.milind.springbootsample")
 public class SpringBootSampleApplication extends SpringBootServletInitializer {
-
-    /**
-     * For eclipse, jar running
-     * 
-     * @param args
-     */
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootSampleApplication.class, args);
-    }
 
     /**
      * For Web app running as a WAR
